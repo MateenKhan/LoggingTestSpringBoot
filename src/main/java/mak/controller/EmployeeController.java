bec434ca226b546a16304a6c1d754e3479637dfa
Mateen <makjavaprogrammer@gmail.com>
2022-11-27 22:38:41 +0530

    swagger cleanup

--- src/main/java/mak/controller/EmployeeController.java
+++ src/main/java/mak/controller/EmployeeController.java
@@ -8,7 +8,6 @@ import mak.pojo.Employee;
 import mak.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.Optional;
 
 @RestController
@@ -20,17 +19,18 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     @PostMapping
-    public Employee save(@RequestBody Employee employee) {
+    public Employee save(
+            @RequestBody Employee employee) {
         return employeeService.save(employee);
     }
 
     @GetMapping("/{id}")
-    public Optional<Employee> get(@ApiParam(value = "id", required = true)@PathVariable("id") Integer id) {
+    public Optional<Employee> get(@ApiParam(value = "id", required = true) @PathVariable("id") Integer id) {
         return employeeService.get(id);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@ApiParam(value = "id", required = true)@PathVariable("id") Integer id) {
+    public void delete(@ApiParam(value = "id", required = true) @PathVariable("id") Integer id) {
         employeeService.delete(Employee.builder().id(id).build());
     }
 
